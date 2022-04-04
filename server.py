6b39338da67bacc394a0de742f99aaa2dbf4ad87
mcmimichan <chan.mcchelle@gmail.com>
2022-04-03 22:51:22 -0700

    Completion

--- server.py
+++ server.py
@@ -1,40 +1,44 @@
 #import socket module
 from socket import *
 serverSocket = socket(AF_INET, SOCK_STREAM)
+
 #Prepare a server socket
 #Fill in start
 serverPort = 12001
-serverSocket.bind('', serverPort)
+serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 #Fill in end
 
 while True:
     #Establish the connection
+    #print (serverSocket.getsockname())
     print ('Ready to serve...')
     connectionSocket, addr = serverSocket.accept() #Fill in
     try:
-        message = connectionSocket.recv(1024) #Fill in
+        message = connectionSocket.recv(1024).decode() #Fill in
         filename = message.split()[1]
+        print(filename)
         f = open(filename[1:])
-        outputdata = f.read() #Fill in
+        outputdata = f.readlines() #Fill in
         #Send one HTTP header line into socket
         #Fill in start
-        connectionSocket.send("HTTP/1.1 200 OK\n")
+        connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
         #Fill in end
 
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i])
+            connectionSocket.send(outputdata[i].encode())
         connectionSocket.close()
     except IOError:
         #Send response message for file not found
         #Fill in start
-        connectionSocket.send('HTTP/1.1 404 Not Found\n')
-        connectionSocket.send('<html><head></head><body><h1>404 Not FOund>/h1></body></html?\n')
+        connectionSocket.send('HTTP/1.1 404 Not Found\r\n\r\n'.encode())
+        connectionSocket.send('<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n\r\n'.encode())
         #Fill in end
 
         #Close client socket
         #Fill in start
         connectionSocket.close()
         #Fill in end
-    serverSocket.close()
+
+serverSocket.close()
